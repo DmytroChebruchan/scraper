01208e353a34c296c0b5a044fb44a27bcf67ef6a
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-08-01 19:48:58 +0300

    adding tests case

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from request_info import RequestInfo
-import requests
 
 
 def main():
-    request_info = RequestInfo()
+    request_info_dict = {
+        "resource": "work.ua",
+        "position": "Python developer",
+        "experience": [],
+        "min_salary": 1,
+        "max_salary": 2,
+        "locations": [],
+        # "locations": ["Odessa", "Lviv"],
+    }
+    request_info = RequestInfo(**request_info_dict)
     url_generated = request_info.url_generator()
-    # request = requests.get(url=request_info.url_generator())
     print(url_generated)
+
+    # request = requests.get(url=request_info.url_generator())
 
 
 if __name__ == "__main__":
